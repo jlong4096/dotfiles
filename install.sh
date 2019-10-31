a79e97b4bd86d4bc2716e87d4bb258ddd6b0a3e5
James Long <jlong4096@gmail.com>
2019-10-30 22:30:25 -0500

    feat: add config files for sway, ranger, and redshift

--- install.sh
+++ install.sh
@@ -38,6 +38,15 @@ ln -svf $PWD/.vimrc ~/.config/nvim/rc.vim
 mkdir -p ~/.config/kitty
 ln -svf $PWD/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 
+mkdir -p ~/.config/sway
+ln -svf $PWD/.config/sway/config ~/.config/sway/config
+
+mkdir -p ~/.config/ranger
+ln -svf $PWD/.config/ranger/rc.conf ~/.config/ranger/rc.conf
+
+mkdir -p ~.config/redshift
+ln -svf $PWD/.config/redshift/redshift.conf ~/.config/redshift/redshift.conf
+
 if [[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
